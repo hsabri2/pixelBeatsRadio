c7efbc2cc74482d9b26e0b35fb6f8962718079df
hsabri2 <huzayfasabri2001@gmail.com>
2023-11-01 23:28:23 -0400

    Adding EJS Syntax and Data passing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 3000;
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
