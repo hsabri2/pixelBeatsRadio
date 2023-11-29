a1f7835ed7502b71906fb4925423d186b7ca21a1
OmarSha30 <odessouk@gmail.com>
2023-11-29 16:16:50 -0500

    listener update

--- public/Listener/script.js
+++ public/Listener/script.js
@@ -5,21 +5,121 @@ elem.style.fontSize = "30px";
 elem.style.fontFamily = "times-new-roman";
 elem.style.color = "aqua";
 
+//contact link
+document.getElementById("contactLink").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default link behavior
+  alert("Thank you for your interest! Please contact us at pixelbeatsradio@mail.com."); // Display the alert message
+});
+
+// Array of preferences
+const preferences = ["Rock", "EDM", "Favorite DJs", "Favorite Songs", "Country", "Pop", "Desi"];
+
+// Function to create options for the dropdown based on the preferences array
+function populateDropdown() {
+    const preferenceDropdown = document.getElementById('preferenceDropdown');
+    preferenceDropdown.innerHTML = '';
+
+    preferences.forEach(preference => {
+        const option = document.createElement('option');
+        option.value = preference;
+        option.textContent = preference;
+        preferenceDropdown.appendChild(option);
+    });
+}
+
+//Function to filter dropdown options based on search input
+function filterOptions() {
+    const searchInput = document.getElementById('searchInput');
+    const searchTerm = searchInput.value.toLowerCase();
+    const preferenceDropdown = document.getElementById('preferenceDropdown');
+    const options = preferenceDropdown.getElementsByTagName('option');
+
+    for (let option of options) {
+        const text = option.textContent.toLowerCase();
+        if (text.includes(searchTerm)) {
+            option.style.display = 'block';
+        } else {
+            option.style.display = 'none';
+        }
+    }
+}
+
+// Event listeners for search input and add/delete buttons
+function addPreference() {
+  const searchInput = document.getElementById('searchInput');
+  const newPreference = searchInput.value.trim(); // Get the value from the search bar
+
+  if (newPreference !== '') {
+      
+      preferences.push(newPreference);
+      alert("Adding New Preference: " + newPreference)
+      populateDropdown();
+      searchInput.value = ''; // Clear the search input after adding
+  }
+}
+
+//alert(`Adding "${selectedOption}" to preferences.`);
+document.getElementById('add-pref').addEventListener('click', addPreference);
+
+
+function deletePreference() {
+  const preferenceDropdown = document.getElementById('preferenceDropdown');
+  const selectedOption = preferenceDropdown.options[preferenceDropdown.selectedIndex].value;
+
+  const index = preferences.indexOf(selectedOption);
+  if (index !== -1) {
+      alert("Deleting Preference: " + selectedOption)
+      preferences.splice(index, 1);
+      populateDropdown();
+  }
+}
+//alert(`Deleting "${selectedOption}" from preferences.`);
+document.getElementById('del-pref').addEventListener('click', deletePreference);
+
+
+// Populate the dropdown initially
+populateDropdown();
+
+
+
+
+
 //play/stop message
-const play = document.querySelectorAll(".play-button");
-const stop = document.querySelectorAll(".stop-button");
+const playbutton = document.querySelectorAll(".play-button");
+const stopbutton = document.querySelectorAll(".stop-button");
 
-play.forEach(function(button) {
-button.addEventListener("click", function() {
-    alert('Playing Song ....');
+let audio = new Audio();
+
+const mp3s = [
+  "Friendship.mp3", "WOT.mp3", "Yazal.mp3", "Zakartuka.mp3"
+];
+
+let randomIndex = -1;
+var randomSong = mp3s[randomIndex];
+
+playbutton.forEach(function(button, index) {
+  button.addEventListener("click", function() {
+
+    if (audio.paused || audio.ended) {
+      randomIndex = Math.floor(Math.random() * mp3s.length);
+    }
+    randomSong = mp3s[randomIndex];
+    audio.src = randomSong;
+    audio.play();
+    alert("Playing " + randomSong);
   });
 });
 
-stop.forEach(function(button) {
-button.addEventListener("click", function() {
-    alert('Song Stopped ....');
+stopbutton.forEach(function(button, index) {
+  button.addEventListener("click", function() {
+    alert("Stopping: "+ randomSong);
+    audio.pause();
+    audio.currentTime = 0;
+    randomSong = mp3s[randomIndex];
   });
 });
+
+
 
 //function to add song
 function addSong(song){
